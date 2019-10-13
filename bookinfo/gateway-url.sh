b2277658f4ac7363488242d6d5346733029bff4e
shaweiguo <shaweiguo@ymail.com>
2019-10-13 22:59:24 +0800

    gateway-url.sh updated

--- bookinfo/gateway-url.sh
+++ bookinfo/gateway-url.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 kubectl -n istio-demo exec -it $(kubectl get pod -n istio-demo -l app=ratings -o jsonpath='{.items[0].metadata.name}') -c ratings -- curl productpage:9080/productpage | grep -o "<title>.*</title>"
 # Determine the ingress IP and port
+export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
